9b653cc0ba9929bfea0f886159c47782a1ef6918
Raphael Jorel <raphael.jorel@laposte.net>
2024-03-19 10:37:00 +0100

    Remove custom dev server entrypoint file

--- server.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-if ($uri !== '/' && file_exists(__DIR__ . '/www' . $uri)) {
-    return false; // Serve the requested resource as-is.
-}
-
-require_once __DIR__ . '/www/index.php';
